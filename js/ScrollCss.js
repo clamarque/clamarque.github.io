1c2969176741bd8a285c894742bb8997598baa46
Charles Lamarque <chabox59@gmail.com>
2015-03-31 23:31:39 +0200

    Update

--- js/ScrollCss.js
+++ js/ScrollCss.js
@@ -4,8 +4,8 @@
  
         if( y > 500 )
         {
-            $(".navbar-default").css({'background-color':'rgb(255,255,255)'});
-            $(".navbar-default .navbar-nav > li > a").css({'color':'#000'});
+            $(".navbar-default").css({'background-color':'transparent'});
+            $(".navbar-default .navbar-nav > li > a").css({'color':'#FFF'});
             //$('#navbar').animate({ 'height': '60'}, {duration : 200, queue : false});
         } else {
             $(".navbar-default").css({'background-color':'transparent'});
