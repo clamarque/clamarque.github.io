f9ffc0a1439a3268e86829d9abdec50d354f8529
Clamarque <chabox59@gmail.com>
2016-01-19 15:09:54 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -156,3 +156,15 @@ $(document).ready(function(){
     });
 });
     
+(function($){
+
+    $('#header__icon').click(function(e){
+        e.preventDefault();
+        $('body').toggleClass('with--sidebar');
+    });
+
+    $('#site-cache').click(function(e){
+        $('body').removeClass('with--sidebar');
+    })
+
+})(jQuery);
